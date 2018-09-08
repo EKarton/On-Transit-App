6be32c7f78518c54f140db5bef1afa195927ae89
Emilio Kartono <emilio.kartono@hotmail.com>
2018-09-08 01:25:28 -0400

    Placed pre-processing aggregators in a new folder

---
+++ backend/Data Aggregator/src/final-processing-aggregators/trip-schedules-aggregator.js
@@ -0,0 +1,10 @@
+class TripSchedulesAggregator{
+    constructor(oldDatabase, newDatabase){
+        this._oldDatabase = oldDatabase;
+        this._newDatabase = newDatabase;
+    }
+
+    processData(){
+        
+    }
+}
