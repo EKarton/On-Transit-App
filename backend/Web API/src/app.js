6c275df90dd3c8fde79f73b1bac97d41790f7d0a
Emilio Kartono <emilio.kartono@hotmail.com>
2018-09-09 22:03:14 -0400

    Added all server files with new connection strings to Heroku

--- backend/Web API/src/app.js
+++ backend/Web API/src/app.js
@@ -130,6 +130,6 @@ app.get("/api/v1/vehicles", (request, response) => {
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
