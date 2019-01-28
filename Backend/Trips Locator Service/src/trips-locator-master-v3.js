8d1476ce2aba715ca17e55874cee8103bd98efcb
Emilio K <emiliokartono@gmail.com>
2019-01-27 22:09:51 -0500

    Changed the algorithm where it is much faster now

--- Backend/Trips Locator Service/src/trips-locator-master-v3.js
+++ Backend/Trips Locator Service/src/trips-locator-master-v3.js
@@ -1,8 +1,6 @@
 const Database = require("on-transit").Database;
 const Location = require("on-transit").Location;
-const PathLocationsTree = require("on-transit").PathLocationTree;
 const Config = require("./res/config");
-const Process = require("process");
 
 class TripsLocatorMasterV3 {
 
@@ -30,49 +28,86 @@ class TripsLocatorMasterV3 {
      * 
      * Each trip must cover the same number of stops (hence the "n" param).
      * 
+     * It will return an object:
+     * {
+     *      "[i, i + 1]": [a1, a2, ..., an],
+     *      ...
+     * }
+     * 
+     * where each key represents a set of two adjacent stops that the user can be in,
+     * and each value are indexes to the trips that contains that possible adjacent stops.
+     * 
      * @param {Object[][]} times The times per trip
      * @param {Integer} time The current time in seconds
      */
     getStopRangesByTime(times, time){
         return new Promise(async (resolve, reject) => {
-            let stopRangesPerTrip = [];
+            let stopRangeToTrips = {};
             for (let i = 0; i < times.length; i++){
                 for (let j = 0; j < times[i].length - 1; j++){
                     let stopTimeA = times[i][j];
                     let stopTimeB = times[i][j + 1];
 
                     if (stopTimeA[1] <= time && time <= stopTimeB[0]){
-                        let newStopRange = [j, j + 1];
-                        stopRangesPerTrip.push(newStopRange);
+                        if (stopRangeToTrips[j] === undefined){
+                            stopRangeToTrips[j] = [i];
+                        }
+                        else{
+                            stopRangeToTrips[j].push(i);
+                        }
                     }
                 }
             }
-            resolve(stopRangesPerTrip);
+            resolve(stopRangeToTrips);
         });
     }
 
     /**
-     * Determines a set of two adjacent stop locations the user with location 'location' can be in.
+     * Get the most recent stop visited in a trip based on the current time.
+     * The times[] contains the times which the bus / train will visit each stop,
+     * where it has the format:
+     * [ [A1, D1], [A2, D2], ..., [An, Dn] ]
+     * 
+     * where [Ai, Di] contains the arrival time 'Ai' and the departure time 'Di'
+     * for stop 'i'.
+     * 
+     * It will return the index to a stop (in the case above, it could be 'i').
+     * 
+     * @param {Integer[][]} times The stop times for the trip
+     * @param {Integer} curTime The current time
+     * @returns {Integer} Index to the most recently visited stop.
+     */
+    getRecentStopsVisitedByTime(times, curTime){
+        return new Promise(async (resolve, reject) => {
+            let possibleStop = -1;
+            for (let i = 0; i < times.length - 1; i++){
+                let stopA = times[i];
+                let stopB = times[i + 1];
+
+                if (stopA[1] <= curTime && curTime <= stopB[0]){
+                    possibleStop = i;
+                    break;
+                } 
+            }
+            resolve(possibleStop);
+        });
+    }
+
+    /**
+     * Determines a set of stops that the user might most recently visit based on the user's location.
      * The 'locationIDs' must be in the format:
      * [ S1, S2, ..., Sn ]
      * 
      * where S1 is the stop location ID for the first stop, S2 is the stop location ID for the second
      * stop, ..., Sn is the stop location ID for the last stop.
      * 
-     * It will return a list of lists in the format:
-     * [
-     *      [A1, A1 + 1],
-     *      [A2, A2 + 1],
-     *      ...
-     *      [Am, Am + 1]
-     * ]
+     * It will return a set of indexes to locationIDs[] that could be the most recently visited stop
      * 
-     * where [Ai, Ai + 1] is a possible pair of two adjacent stop locations. 
      * @param {String[]} locationIDs A list of stop location IDs
      * @param {Location} location The current location
-     * @returns {Integer[][]} The stop locations
+     * @returns {Set} The stop locations
      */
-    getStopRangesByLocation(locationIDs, location){
+    getRecentStopsVisitedByLocation(locationIDs, location){
         return new Promise(async (resolve, reject) => {
 
             let jobs = [];
@@ -80,8 +115,6 @@ class TripsLocatorMasterV3 {
                 let newJob = new Promise(async (resolveJob, rejectJob) => {
                     let locationID_1 = locationIDs[i];
                     let locationID_2 = locationIDs[i + 1];
-
-                    console.log("Location ID 1:" + locationID_1);
                     
                     let request1 = this.database.getObject("stop-locations", { "_id": locationID_1 });
                     let request2 = this.database.getObject("stop-locations", { "_id": locationID_2 });
@@ -94,22 +127,101 @@ class TripsLocatorMasterV3 {
                     let dy = location_1.latitude - location_2.latitude;
                     let lengthOfLineSquared = (dx * dx + dy * dy);
                     let innerProduct = (location.longitude - location_2.longitude) * dx + 
-                                        (location.latitude - location_2.latitude) * dy;
+                                       (location.latitude - location_2.latitude) * dy;
                     
                     let isProjectionInLine = 0 <= innerProduct && innerProduct <= lengthOfLineSquared;
 
                     if (isProjectionInLine){
-                        resolveJob([i, i + 1]);
+                        resolveJob(i);
                     }
                     else{
-                        resolveJob([]);
+                        resolveJob(null);
                     }
                 });
                 jobs.push(newJob);
             }
-            let rawResults = await Promise.all(jobs);
-            let finalResults = rawResults.filter(a => a.length == 2);
-            resolve(finalResults);
+            let possibleStops = await Promise.all(jobs);
+            possibleStops = possibleStops.filter(a => a !== null);
+            let possibleStopsSet = new Set(possibleStops);
+            resolve(possibleStopsSet);
+        });
+    }
+
+    /**
+     * Get a subset from the list of possible schedules the user might be on based on
+     * the user's location and current time.
+     * 
+     * It will return a subset of the 'schedules' list as a set. 
+     * @param {String[]} schedules A list of possible schedules
+     * @param {Integer} time The time in seconds from midnight
+     * @param {Location} location The location
+     * @returns {Set} A set of schedules the user might be in.
+     */
+    getPossibleSchedules(tripScheduleIDs, time, location){
+        return new Promise(async (resolve, reject) => {
+
+            let possibleSchedules = new Set();
+            let schedulesAggregatorCursor = await this.database.getAggregatedObjects("schedules", [
+                {
+                    $match: {
+                        $and: [
+                            { _id: { $in: tripScheduleIDs } },
+                            { startTime: { $lte: time } },
+                            { endTime: { $gte: time } } 
+                        ]
+                    }
+                },
+                {
+                    $group: {
+                        _id: {
+                            "headsigns": "$headsigns",
+                            "locationIDs": "$locationIDs"
+                        },
+                        times: { $push: "$times" },
+                        scheduleIDs: { $push: "$_id" }
+                    }
+                }
+            ]);
+            while (await schedulesAggregatorCursor.hasNext()){
+                let aggregatedSchedule = await schedulesAggregatorCursor.next();
+
+                // console.log(" ====================== AGGREGATED SCHEDULE ==============================");
+                // console.log(JSON.stringify(aggregatedSchedule));
+
+                let times = aggregatedSchedule.times;
+                let locationIDs = aggregatedSchedule._id.locationIDs;
+                let scheduleIDs = aggregatedSchedule.scheduleIDs;
+
+                let stopRangesByLocation = await this.getRecentStopsVisitedByLocation(locationIDs, location);
+
+                // console.log("Recent stops visited by location");
+                // console.log(JSON.stringify(Array.from(stopRangesByLocation)));
+
+                // console.log("WASD:");
+                // console.log(await this.getStopRangesByTime(times, time));
+
+                for (let i = 0; i < times.length; i++){
+                    let tripSchedule = times[i];
+                    let tripScheduleID = scheduleIDs[i];
+                    let recentStopVisitedByTime = await this.getRecentStopsVisitedByTime(tripSchedule, time);
+
+                    // console.log("Recent stops visited per schedule by time: " + recentStopVisitedByTime);
+
+                    if (recentStopVisitedByTime >= 0){
+                        if (stopRangesByLocation.has(recentStopVisitedByTime)){
+                            possibleSchedules.add(tripScheduleID);
+                        }
+                        else if (stopRangesByLocation.has(recentStopVisitedByTime - 1)){
+                            possibleSchedules.add(tripScheduleID);
+                        }
+                        else if (stopRangesByLocation.has(recentStopVisitedByTime + 1)){
+                            possibleSchedules.add(tripScheduleID);
+                        }
+                    }
+                }
+            }
+
+            resolve(possibleSchedules);
         });
     }
 
@@ -125,6 +237,8 @@ class TripsLocatorMasterV3 {
         return new Promise(async (resolve, reject) => {
             let latitude = location.latitude;
             let longitude = location.longitude;
+            // console.log("Current time:" + time);
+            // console.log("Location: " + JSON.stringify(location));
 
             let responseObj = {};
 
@@ -149,64 +263,26 @@ class TripsLocatorMasterV3 {
                 });
                 while (await tripsCursor.hasNext()){
                     let trip = await tripsCursor.next();
+
+                    // console.log(" ============================ TRIP =================================");
+                    // console.log(JSON.stringify(trip));
+
                     let schedules = trip.schedules;
+                    let possibleScheduleIDs = await this.getPossibleSchedules(schedules, time, location);
 
-                    let schedulesAggregatorCursor = await this.database.getAggregatedObjects("schedules", [
-                        {
-                            $match: {
-                                "_id": { $in: schedules },
-                                "startTime": { $lte: time },
-                                "endTime": { $gte: time }
-                            }
-                        },
-                        {
-                            $group: {
-                                _id: {
-                                    "headsigns": "$headsigns",
-                                    "locationIDs": "$locationIDs"
-                                },
-                                times: { $push: "$times" },
-                            }
-                        }
-                    ]);
-                    while (await schedulesAggregatorCursor.hasNext()){
-                        let aggregatedSchedule = await schedulesAggregatorCursor.next();
-                        console.log(JSON.stringify(aggregatedSchedule));
+                    if (possibleScheduleIDs.size > 0){
 
-                        let times = aggregatedSchedule.times;
-                        let locationIDs = aggregatedSchedule._id.locationIDs;
-
-                        let stopRangesByLocation_Promise = this.getStopRangesByLocation(locationIDs, location);
-                        let stopRangesByTime_Promise = this.getStopRangesByTime(times, time);
-
-                        let results = await Promise.all([stopRangesByLocation_Promise, stopRangesByTime_Promise]);
-                        let stopRangesByLocation = results[0];
-                        let stopRangesByTime = results[1];
-
-                        console.log("Stop Ranges by Location:");
-                        console.log(stopRangesByLocation);
-
-                        console.log("Stop Ranges by Time:");
-                        console.log(stopRangesByTime);
-
-                        let commonStopRanges = [];
-                        for (let i = 0; i < stopRangesByTime.length; i++){
-                            let curStopRange = stopRangesByTime[i];
-                            let stopRange_1 = stopRangesByLocation.filter(item => item[0] === curStopRange[0] && item[1] === curStopRange[1]);
-                            if (stopRange_1.length > 0){
-                                commonStopRanges.push(curStopRange);
-                            }
-                        }
-                        console.log("The intersection of the two");
-                        console.log(commonStopRanges);
-
-                        if (commonStopRanges.length > 0){
-                            console.log("I AM HERE");
+                        responseObj[trip._id] = {
+                            shortname: trip.shortName,
+                            longname: trip.longName,
+                            headsign: trip.headSign,
+                            type: trip.type,
+                            schedules: Array.from(possibleScheduleIDs)
                         }
                     }
                 }
             }
-            resolve();
+            resolve(responseObj);
         });
     }
 }
