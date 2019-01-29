7826fad4bbbffc4e5f7bae112d12421b63b7b8b6
Emilio K <emiliokartono@gmail.com>
2019-01-28 23:36:21 -0500

    Removed redundent files

--- Frontend/Web App/src/index.js
+++ Frontend/Web App/src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import store from "./stores/index";
-import { addArticle } from "./actions/index";
 
 ReactDOM.render(
     <App/>, 
     document.getElementById('root')
 );
-
-window.store = store;
-window.addArticle = addArticle;
